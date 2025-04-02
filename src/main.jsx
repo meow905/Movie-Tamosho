d2d9df1afcbfc794c8694358ff2b754218a0dcb4
meow905 <tabrezbunny@gmail.com>
2025-04-03 00:50:14 +0500

    someFixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import NotFoundTicket from './components/NotFoundTicket.jsx';
-import MoviesPage from './components/MoviesPage.jsx';
-import { useState } from 'react';
+
 
 const router = createBrowserRouter([
   {
@@ -12,13 +11,13 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/NotFoundTicket",
+    path: "NotFoundTicket",
     element : <NotFoundTicket/>
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
-  
+
   <div className="bg-bgImage bg-top bg-no-repeat bg-fixed min-h-[100vh] overflow-clip">
    
     <RouterProvider router={router}/>
